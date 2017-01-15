3a1901e25c5adddbd1a5eab41e412a78b11fd0a5
Gordon Seto <gordonseto@Gordons-MacBook-Pro.local>
2017-01-15 12:30:24 -0800

    added iframeHTML generation

--- main.js
+++ main.js
@@ -1,26 +1,41 @@
 $('#searchBox').keyup(function(event){
-	//console.log("wat");
+	sendSearchAPIRequest(this.value);
 })
 
 $('#searchBox').keypress(function (event){
 	if (event.which == 13){
 		event.preventDefault();
-		sendSearchAPIRequest(this.value);
 	}
 })
 
+const phBase = 'http://www.pornhub.com/webmasters/';
+
 function sendSearchAPIRequest(searchBoxText){
 	console.log(searchBoxText);
-
-	var url = 'http://www.pornhub.com/webmasters/search?id=44bc40f3bc04f65b7a35&search=big&tags[]=Teen&thumbsize=medium';
+	const baseUrl = phBase + 'search?id=44bc40f3bc04f65b7a35&ordering=mostviewed&search=';
+	const url = baseUrl + searchBoxText;
 
 	$.ajax({
 		type: 'GET',
 		url: url,
 		dataType: 'json',
-		crossDoain: true,
+		crossDomain: true,
 		success: function(data){
 			console.log(data);
+			console.log(data.videos[0].video_id);
+			console.log(data.videos[0].title);
+			if (data.videos.length != 0) {
+				const videoId = data.videos[0].video_id;
+				$("#videoContainer").html(generateIframeHTMLFor(videoId));
+			}	
 		}
 	});
+}
+
+function generateIframeHTMLFor(id){
+	var html = "<iframe src=\"http://www.pornhub.com/embed/" + id 
+				+ "\" frameborder=\"0\" width=\"608\" height=\"468\""
+				+ " scrolling=\"no\" autoplay=\"1\"> </iframe>";
+	console.log(html);
+	return html
 }
